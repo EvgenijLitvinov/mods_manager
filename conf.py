0d821aed3b5eab438f679bf1c350fcf0c9b5f7d6
Jack <litvinov.e.v@gmail.com>
2023-02-17 13:55:24 +0200

    mods is list now, not dict

--- conf.py
+++ conf.py
@@ -1,27 +1,37 @@
 MODSDIR = r'D:\Lesta\Tanki\mods\\'[:-1]
 
-mods = {'xvm': {
-            'files': ['com.modxvm.xfw\\']
-        },
-        'Прицел от Jimbo': {
-            'files': ['crosshair_arcade', 'crosshair_Controls',\
-                      'crosshair_sniper', 'crosshair_strategic', 'tv.protanki.sights']
-        },
-        'Отметки на стволах': {
-            'files': ['Marks_On_Gun']
-        },
-        'Белые трупы': {
-            'files': ['wTanks', 'wTracks']
-        },
-        'Анализ брони в ангаре': {
-            'files': ['izeberg.modsettingsapi_1.5.4', 'poliroid.modslistapi_1.4.3', 'tv.protanki.reducedarmor_3.4.3']
-        },
-        'Боевые раны': {
-            'files': ['poliroid.battlehits_1.8.5', 'poliroid.modslistapi_1.4.3']
-        },
-        'Процент отметки': {
-            'files': ['me.poliroid.gunmarks-lebwa_3.1.2']
-        },
-        'Время сведения': {
-            'files': ['izeberg.modsettingsapi_1.5.4', 'poliroid.modslistapi_1.4.3', 'tv.protanki.reductioninfo_2.8.3']
-        }}
+mods = [
+    {
+    'name': 'xvm',
+    'files': ['com.modxvm.xfw\\']
+    },
+    {
+    'name': 'Прицел от Jimbo',
+    'files': ['crosshair_arcade', 'crosshair_Controls',\
+              'crosshair_sniper', 'crosshair_strategic', 'tv.protanki.sights']
+    },
+    {
+    'name': 'Отметки на стволах',
+    'files': ['Marks_On_Gun']
+    },
+    {
+    'name': 'Белые трупы',
+    'files': ['wTanks', 'wTracks']
+    },
+    {
+    'name': 'Анализ брони в ангаре',
+    'files': ['izeberg.modsettingsapi_1.5.4', 'poliroid.modslistapi_1.4.3', 'tv.protanki.reducedarmor_3.4.3']
+    },
+    {
+    'name': 'Боевые раны',
+    'files': ['poliroid.battlehits_1.8.5', 'poliroid.modslistapi_1.4.3']
+    },
+    {
+    'name': 'Процент отметки',
+    'files': ['me.poliroid.gunmarks-lebwa_3.1.2']
+    },
+    {
+    'name': 'Время сведения',
+    'files': ['izeberg.modsettingsapi_1.5.4', 'poliroid.modslistapi_1.4.3', 'tv.protanki.reductioninfo_2.8.3']
+    }
+]
