06dacdcca3b110f28ff4a422f76f766ba8999d26
Jack <litvinov.e.v@gmail.com>
2023-03-11 22:11:40 +0200

    os.path --> pathlib

--- base.py
+++ base.py
@@ -1,4 +1,4 @@
-import os
+#import os
 import json
 import PySimpleGUI as sg
 from urllib.request import urlopen, Request
@@ -8,22 +8,18 @@ from pathlib import Path
 with open('conf.json', encoding='utf-8') as fp:
     data = json.load(fp)
 GAMEDIR, mods = data
-with open(os.path.join(GAMEDIR, 'version.xml')) as px:               # game version
+with open(Path(GAMEDIR, 'version.xml')) as px:               # game version
     for _ in range(3):
         VERSION = px.readline()[13:-18]
-MODSDIR = os.path.join(GAMEDIR, 'mods', VERSION)
-MODSLIST = os.listdir(MODSDIR)
+MODSDIR = Path(GAMEDIR, 'mods', VERSION)
+#MODSLIST = os.listdir(MODSDIR)
 
 sg.theme('PythonPlus')
 
-def ch(mod):                                        # enable or disable
-    if mod['files'][0] + '.wotmod' in MODSLIST:
-        return True
-
 def my_color(mod):                                  # mod's color at mods list
-    if mod['files'][0] in map(lambda x: x[:-7], MODSLIST):
+    if real_f.exists():
         if not mod['date']:
-            mod['date'] = datetime.fromtimestamp(os.path.getctime(mod['files'][0])).strftime('%d %b %Y')
+            mod['date'] = datetime.fromtimestamp(Path.stat(real_f).st_ctime).strftime('%d %b %Y')
         return 'white'
     else:
         if not mod['date']:
@@ -43,9 +39,12 @@ layout = [[sg.Text('Папка c игрой:')],
           [sg.Input(default_text=GAMEDIR), sg.FolderBrowse()],
           [sg.Text('Версия игры:   '), sg.Text(VERSION)]]
 for mod in mods:
-    tmp = [sg.Check('', default=ch(mod), key=mod["name"])]                  # checkbutton
-    tmp += [sg.Text(mod["name"], text_color=my_color(mod))]                 # mod name
-    tmp += [sg.Button('upd', key=mod["name"])] * upd(mod)                   # update button
+    real_f = Path('}|{ek@')
+    for file in MODSDIR.glob(f"{mod['files'][0]}*"):
+        real_f = file
+    tmp = [sg.Check('', default=real_f.suffix == '.wotmod', key=mod["name"])]     # checkbutton
+    tmp += [sg.Text(mod["name"], text_color=my_color(mod))]                     # mod name
+    tmp += [sg.Button('upd', key=mod["name"])] * upd(mod)                       # update button
     layout += [tmp]
 layout += [[sg.Button('Применить'), sg.Button('Обновить все')],
            [sg.Button('CLOSE', button_color='red')]]
