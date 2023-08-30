76fa3ff0d5ff6c882b4095a8d07cb796775b658b
Jack <litvinov.e.v@gmail.com>
2023-08-30 19:21:37 +0300

    prosto commit

--- base.py
+++ base.py
@@ -1,7 +1,7 @@
 #import os
 import json
 import PySimpleGUI as sg
-from urllib.request import urlopen, Request, urlretrieve
+#from urllib.request import urlopen, Request, urlretrieve
 import requests
 from datetime import datetime
 from pathlib import Path
@@ -20,48 +20,21 @@ MODSDIR = Path(GAMEDIR, 'mods', VERSION)
 sg.theme('PythonPlus')
 
 def foo(mod):                                               # for rendering
-    for file in mod['files']:
-        real_f = tuple(MODSDIR.glob(f'{file}*'))
-        if not real_f:
-            return False, 'black', True                     # check, color, upd
-    check = real_f[0].suffix == '.wotmod'
     url = mod['url']                                        # Last-Modified
     if mod['name'] == 'Боевые раны':
         soup = BeautifulSoup(requests.get(url, stream=True).content, 'lxml')
         s = soup.find('a', 'down_new')['href']
         url = s[s.find('https') : s.find('&')]
         mod['dwn'] = url
+    for file in mod['files']:
+        real_f = tuple(MODSDIR.glob(f'{file}*'))
+        if not real_f:
+            return False, 'black', True                     # check, color, upd
+    check = real_f[0].suffix == '.wotmod'
     last_m = requests.get(url, stream=True).headers['last-modified']
     create_f = datetime.fromtimestamp(Path.stat(real_f[0]).st_ctime).strftime('%d %b %Y')
-    if datetime.strptime(last_m[5:16], '%d %b %Y') > create_f:
-        upd = True
-    else:
-        upd = False
+    upd = datetime.strptime(last_m[5:16], '%d %b %Y') > create_f
     return check, 'white', upd                              # check, color, upd
-
-def my_color(mod):                                  # mod's color at mods list
-    if real_f.exists():
-        if not mod['date']:
-            mod['date'] = datetime.fromtimestamp(Path.stat(real_f).st_ctime).strftime('%d %b %Y')
-        return 'white'
-    else:
-        if not mod['date']:
-            mod['date'] = '01 Jan 2023'
-        return 'black'
-
-def upd(mod):                                       # Last-Modified
-    url = mod['url']
-    if 'cls' in mod:
-        soup = BeautifulSoup(requests.get(url, stream=True).content, 'lxml')
-        s = soup.find('a', mod['cls'])['href']
-        url = s[s.find('https') : s.find('&')]
-        mod['url0'] = url
-    res = requests.get(url, stream=True).headers['last-modified']
-    if datetime.strptime(res[5:16], '%d %b %Y') > datetime.strptime(mod['date'], '%d %b %Y'):
-        mod['flag'] = True
-        return True
-    else:
-        return False
 
 def mod_version(mod):                               # version of mod
     if mod['name'] == 'xvm':
@@ -74,10 +47,9 @@ def mod_version(mod):                               # version of mod
 # ------------------------------- download & install mod -------------------    
 def inst(mod):
     sg.popup_ok(f'Installing {mod["name"]}', background_color='red', no_titlebar=True)
-    if 'url0' in mod:
-        url = mod['url0']
-    else:
-        url = mod['url']
+    url = mod['url']
+    if 'dwn' in mod:
+        url = mod['dwn']
     tmparj = Path('tmparj')
     tmparj.write_bytes(requests.get(url, stream=True).content)
     if not 'pathes' in mod:
@@ -92,19 +64,15 @@ def inst(mod):
                 Path(MODSDIR, p.parts[-1]).write_bytes(p.read_bytes())
         rmtree('oops')
     tmparj.unlink()
-    mod['flag'] = False
-    mod['date'] = datetime.now().strftime('%d %b %Y')
 # -------------------------------------------------------------------------------
 layout = [[sg.Text('Папка c игрой:')],
           [sg.Input(default_text=GAMEDIR), sg.FolderBrowse()],
           [sg.Text('Версия игры:   '), sg.Text(VERSION)]]
 for mod in mods:
-    real_f = Path('}|{ek@')
-    for file in MODSDIR.glob(f"{mod['files'][0]}*"):
-        real_f = file
-    tmp = [sg.Check('', default=real_f.suffix == '.wotmod', key=mod["name"])]     # checkbutton
-    tmp += [sg.Text(mod["name"], text_color=my_color(mod))]                     # mod name
-    tmp += upd(mod) * [sg.Button('upd', key=mod["name"]), sg.Text(mod_version(mod))]   # update button
+    check, color, upd = foo(mod)
+    tmp = [sg.Check('', default=check, key=mod["name"])]                            # checkbutton
+    tmp += [sg.Text(mod["name"], text_color=color)]                                 # mod name
+    tmp += upd * [sg.Button('upd', key=mod["name"]), sg.Text(mod_version(mod))]     # update button
     layout += [tmp]
 layout += [[sg.Button('Применить'), sg.Button('Обновить все')],
            [sg.CloseButton('CLOSE', button_color='red')]]
@@ -117,6 +85,7 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     if event == 'Применить':
+        print(event)
         continue
     if event == 'Обновить все':
         for mod in mods:
@@ -130,8 +99,10 @@ while True:
             inst(mod)
             window[event].update(visible=False)
 
+"""
 data = GAMEDIR, ARCH, mods
 with open('conf.json', 'w', encoding='utf-8') as fp:
     data = json.dump(data, fp, ensure_ascii=False, indent=4)
+"""
 
 window.close()
